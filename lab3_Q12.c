609aa26533c5687ce45221aba7477835239a12ef
dipto <diptadip2105@gmail.com>
2021-08-10 10:17:54 +0530

    lab3

--- lab3_Q12.c
+++ lab3_Q12.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void printMatrix(matrix);
-matrix* matrixMultiply(matrix*,matrix*);
-matrix fillMatrix();
+
 
 typedef struct Matrix{
     int rows;
     int cols;
     int**arr;
 }matrix;
+
+void printMatrix(matrix);
+matrix matrixMultiply(matrix,matrix);
+matrix fillMatrix();
 
 int main()
 {
@@ -24,8 +26,8 @@ int main()
     printMatrix(b);
 
     printf("Multiplying...\n");
-    matrix* c=matrixMultiply(&a,&b);
-    printMatrix(*c);
+    matrix c=matrixMultiply(a,b);
+    printMatrix(c);
 
 }
 matrix fillMatrix()
@@ -45,20 +47,24 @@ matrix fillMatrix()
     }
     return m;
 }
-matrix* matrixMultiply(matrix* a,matrix* b)
+matrix matrixMultiply(matrix a,matrix b)
 {
-    matrix* c;
-    if(a->cols != b->rows){
+    matrix c;
+    if(a.cols != b.rows){
         printf("Incompatible...\n");
         exit(0);
     }
-    c->rows=a->rows;
-    c->cols=b->cols;
-    for(int i=0;i<c->rows;i++){
-        for(int j=0;j<c->cols;j++){
-            c->arr[i][j]=0;
-            for(int k=0;k<a->cols;k++){
-                c->arr[i][j]+=a->arr[i][k]*b->arr[k][j];
+    c.rows=a.rows;
+    c.cols=b.cols;
+    c.arr=(int**)malloc(c.rows*sizeof(int*));
+    for(int i=0;i<c.rows;i++){
+        c.arr[i]=(int*)malloc(c.cols*sizeof(int));
+    }
+    for(int i=0;i<c.rows;i++){
+        for(int j=0;j<c.cols;j++){
+            c.arr[i][j]=0;
+            for(int k=0;k<a.cols;k++){
+                c.arr[i][j]+=a.arr[i][k]*b.arr[k][j];
             }
         }
     }
