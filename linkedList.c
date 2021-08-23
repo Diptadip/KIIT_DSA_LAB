c0d986b30e99e781dd7d6264dd0984694bec15ca
dipto <diptadip2105@gmail.com>
2021-08-23 16:02:42 +0530

    singly linked list

--- linkedList.c
+++ linkedList.c
@@ -44,9 +44,79 @@ void insertAfter(struct Node** head,int newData,int n)
         addAtHead(head,newData);
     }
 }
-/*void deleteNode(struct Node **head, int n){
+int countNodes(struct Node* ptr)
+{
+    int count=0;
+    while(ptr){
+        count++;
+        ptr=ptr->next;
+    }
+    return count;
+}
+void deleteHead(struct Node **head)
+{
+    struct Node *ptr=NULL;
+    if(*head==NULL){
+        printf("List is empty");
+        return;
+    }
+    ptr=(*head)->next;
+    printf("deleted %d ",(*head)->data);
+    free((void*)(*head));
+    *head=ptr;
+}
+void deleteTail(struct Node **head)
+{
+    struct Node* p=NULL;
+    struct Node* q=NULL;
+    if((*head)==NULL || (*head)->next==NULL){
+        deleteHead(head);
+    }
+    else{
+        for(p=*head;p->next;p=p->next){
+            q=p;
+        }
+        printf("Deleting Node %d ",p->data);
+        free((void*)p);
+        q->next=NULL;    
+    }
+}
+void deleteAllNodes(struct Node**head)
+{
+    struct Node *ptr=NULL;
+    while(*head){
+        ptr=*head;
+        *head=(*head)->next;
+        printf("Deleting node %d\n",ptr->data);
+        free((void*)ptr);
+    }
+}
+void deletePos(struct Node**head , int pos)
+{
+    struct Node* p=*head;
+    struct Node* q=NULL;
+    if(!*head){
+        printf("List is empty ");
+    }
+    else if(pos==0){
+        deleteHead(head);
+    }
+    else{
+        for(int i=0;p && i<pos-1;i++){
+            p=p->next;
+        }
+        if(!p || !p->next){
+            printf("position out of bound\n");
+        }
+        else{            
+            q=p->next->next;
+            printf("Deleteing node %d\n",p->next->data);
+            free((void*)p->next);
+            p->next=q;
+        }
+    }
+}
 
-}*/
 void printList(struct Node* ptr)
 {
     if(ptr==NULL)
@@ -65,7 +135,11 @@ int main()
         printf("2. Insert at tail\n");
         printf("3. Insert after\n");
         printf("4. print List\n");
-        printf("5. exit\n");
+        printf("5. delete head\n");
+        printf("6. delete tail\n");
+        printf("7. Delete All nodes\n");
+        printf("8. Delete specific pos\n");
+        printf("9. exit\n");
         printf("Enter your choice: ");
         scanf("%d",&choice);
 
@@ -95,8 +169,26 @@ int main()
                 printList(head);
                 printf("\n");
                 break;
-            
+
             case 5:
+                deleteHead(&head);
+                break;
+            
+            case 6:
+                deleteTail(&head);
+                break;
+
+            case 7:
+                deleteAllNodes(&head);
+                break;
+
+            case 8:
+                printf("Enter Node number you want to delete (starting from 0): ");
+                scanf("%d",&n);
+                deletePos(&head,n);
+                break;
+            
+            case 9:
                 printf("Exitting code\n");
                 exit(0);
 
